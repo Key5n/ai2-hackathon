0de8512f38821f7ee9ccad3bf03429c22e39e48b
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-11-25 10:37:01 +0900

    サイドバーのテスト用リンクの修正

--- src/features/ui/Sidebar/Sidebar.tsx
+++ src/features/ui/Sidebar/Sidebar.tsx
@@ -8,19 +8,22 @@ export function Sidebar() {
         <AccordionItem title="開催中">
           <ul>
             <li>
-              <SidebarItem link="/hoge">知能系演習</SidebarItem>
+              <SidebarItem link="/1/test">知能系演習</SidebarItem>
             </li>
             <li>
-              <SidebarItem link="/hoge">知能系演習</SidebarItem>
-            </li>
-            <li>
-              <SidebarItem link="/hoge">知能系演習</SidebarItem>
+              <SidebarItem link="/2/test">メディア系演習</SidebarItem>
             </li>
           </ul>
         </AccordionItem>
       </Accordion>
       <Accordion isCompact>
-        <AccordionItem title="終了済み"></AccordionItem>
+        <AccordionItem title="終了済み">
+          <ul>
+            <li>
+              <SidebarItem link="/3/test">理系基礎演習</SidebarItem>
+            </li>
+          </ul>
+        </AccordionItem>
       </Accordion>
     </div>
   );
